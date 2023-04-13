3b0c1a7f0e6f563d60ca24ea7a47b2cc7ef1925b
DimaHovrov <DimaHovrov@example.com>
2023-04-13 23:55:53 +0900

    Refactoring handlers.py

---
+++ general/conversation_handlers.py
@@ -0,0 +1,34 @@
+from telegram.ext import (CommandHandler, MessageHandler,
+                          Filters, CallbackQueryHandler, 
+                          ConversationHandler)
+
+import menus.info_messages_menu as info_messages_menu
+import general.patterns_states as p_s
+
+#конверсейшн поиска сообщения по заголовку
+conv_handler_title_search = ConversationHandler(
+        entry_points=[CallbackQueryHandler(
+            info_messages_menu
+            .title_search_icallback, pattern="^" + str(p_s.TITLE_SEARCH_PATTERN) + "$")],
+        states={
+            p_s.TITLE_SEARCH_STATE: [MessageHandler(Filters.text, info_messages_menu.title_search_ccallback)],
+            p_s.CHOOSE_MESSAGE_STATE: [MessageHandler(Filters.regex("^/message_[0-9]+$"), info_messages_menu.choose_message_ccallback)],
+        },
+        fallbacks=[CommandHandler("cancel", info_messages_menu.cancel)],
+        name='TITLE_SEARCH_CONVERSATION',
+        persistent=True
+    )
+
+#конверсейшн поиска сообщения по категории
+conv_handler_category_search = ConversationHandler(
+    entry_points=[CallbackQueryHandler(
+        info_messages_menu
+        .category_search_icallback, pattern="^" + str(p_s.CATEGORY_SEARCH_PATTERN) + "$")],
+    states={
+        p_s.CATEGORY_SEARCH_STATE: [MessageHandler(Filters.text, info_messages_menu.category_search_ccallback)],
+        p_s.CHOOSE_MESSAGE_STATE: [MessageHandler(Filters.regex("^/message_[0-9]+$"), info_messages_menu.choose_message_ccallback)],
+    },
+    fallbacks=[CommandHandler("cancel", info_messages_menu.cancel)],
+    name='CATEGORY_SEARCH_CONVERSATION',
+    persistent=True
+)
--- handlers.py
+++ handlers.py
@@ -1,16 +1,14 @@
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import (Updater, Dispatcher, CommandHandler, MessageHandler,
-                          CallbackContext, Filters, CallbackQueryHandler, ConversationHandler, ContextTypes)
-from telegram import Bot, Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import (CommandHandler, MessageHandler,
+                          CallbackContext, Filters, CallbackQueryHandler)
+from telegram import Update, InlineKeyboardMarkup
 
 import model.User as user_module
-import model.InfoMessage as InfoMessage
-import model as m
-import re
 
 import menus.info_messages_menu as info_messages_menu
 import sud_messages.sud_messages as sud_messages
 import general.patterns_states as p_s
+import general.conversation_handlers as conversations
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -49,35 +47,18 @@ def contact_user(update: Update, context: CallbackContext) -> None:
 
 
 def reg_handlers(dispatcher):
+    reg_commands(dispatcher)
+    reg_callback_querys(dispatcher)
+    reg_conversations(dispatcher)
+
+
+def reg_commands(dispatcher):
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler(
         "menu_messages_info", menu_messages_info))
 
-    conv_handler_title_search = ConversationHandler(
-        entry_points=[CallbackQueryHandler(
-            info_messages_menu
-            .title_search_icallback, pattern="^" + str(p_s.TITLE_SEARCH_PATTERN) + "$")],
-        states={
-            p_s.TITLE_SEARCH_STATE: [MessageHandler(Filters.text, info_messages_menu.title_search_ccallback)],
-            p_s.CHOOSE_MESSAGE_STATE: [MessageHandler(Filters.regex("^/message_[0-9]+$"), info_messages_menu.choose_message_ccallback)],
-        },
-        fallbacks=[CommandHandler("cancel", info_messages_menu.cancel)],
-        name='TITLE_SEARCH_CONVERSATION',
-        persistent=True
-    )
 
-    conv_handler_category_search = ConversationHandler(
-        entry_points=[CallbackQueryHandler(
-            info_messages_menu
-            .category_search_icallback, pattern="^" + str(p_s.CATEGORY_SEARCH_PATTERN) + "$")],
-        states={
-            p_s.CATEGORY_SEARCH_STATE: [MessageHandler(Filters.text, info_messages_menu.category_search_ccallback)],
-            p_s.CHOOSE_MESSAGE_STATE: [MessageHandler(Filters.regex("^/message_[0-9]+$"), info_messages_menu.choose_message_ccallback)],
-        },
-        fallbacks=[CommandHandler("cancel", info_messages_menu.cancel)],
-        name='CATEGORY_SEARCH_CONVERSATION',
-        persistent=True
-    )
+def reg_callback_querys(dispatcher):
     dispatcher.add_handler(CallbackQueryHandler(
         sud_messages.send_info_messages_icallback, pattern="^" + str(p_s.SEND_MESSAGE_PATTERN) + "$"))
     dispatcher.add_handler(CallbackQueryHandler(
@@ -90,6 +71,9 @@ def reg_handlers(dispatcher):
         sud_messages.delete_info_messages_icallback, pattern="^" + str(p_s.DELETE_MESSAGE_PATTERN) + "$"))
     dispatcher.add_handler(CallbackQueryHandler(
         sud_messages.question_delete_icallback, pattern="^q_delete [0-9]+$"))
-    dispatcher.add_handler(conv_handler_title_search)
-    dispatcher.add_handler(conv_handler_category_search)
+
+
+def reg_conversations(dispatcher):
+    dispatcher.add_handler(conversations.conv_handler_title_search)
+    dispatcher.add_handler(conversations.conv_handler_category_search)
     dispatcher.add_handler(MessageHandler(Filters.contact, contact_user))
